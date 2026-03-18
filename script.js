6c71bc1c2511717de97201cda97d451dda5e379d
manarelhamshary <119499988+manarelhamshary@users.noreply.github.com>
2026-03-18 23:44:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const envelopeScreen = document.getElementById("envelopeScreen");
 const inviteScreen = document.getElementById("inviteScreen");
 const music = document.getElementById("music");
 
-envelope.addEventListener("click",()=>{
+envelope.addEventListener("click", ()=>{
+  music.play();
+  envelopeScreen.style.display = "none";
 
-music.play();
-
-envelopeScreen.style.display="none";
-inviteScreen.classList.remove("hidden");
-inviteScreen.classList.add("open-animation");
-
+  setTimeout(()=>{
+    inviteScreen.classList.remove("hidden");
+    inviteScreen.classList.add("open-animation");
+  }, 300); // 0.3 ثانية
 });
